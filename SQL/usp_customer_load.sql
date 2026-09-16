42a405312151d8411853ced006c553d4fab4bc3d
ErickVisone <erickvisone@hotmail.com>
2026-09-16 20:04:56 +0200

    feat: add customer info, plus stored procedure to load from transaction to warehouse

---
+++ SQL/usp_customer_load.sql
@@ -0,0 +1,123 @@
+create procedure usp_load_customer
+
+as
+
+----------------------------------------------
+--           Creating tables 
+----------------------------------------------
+
+-- Creating table customer
+if object_id('staging_procfit.dbo.st_customer') is null
+create table st_customer (
+	cod_cliente			numeric(15)
+	,nome				varchar(100)
+	,nome_fantasia		varchar(100)
+	,cod_classificacao	numeric(15)
+
+);
+
+-- Creating table customer_state
+if object_id('staging_procfit.dbo.st_customer_state') is null
+begin
+
+create table staging_procfit.dbo.st_customer_state (
+	UF			varchar(2)
+	,estado     varchar(60)
+)
+end;
+
+-- Creating table customer_classification
+if object_id('staging_procfit.dbo.st_customer_classification') is null
+begin
+
+create table staging_procfit.dbo.st_customer_classification (
+	cod_classificacao	numeric(15)
+	,descricao			varchar(80)
+)
+end;
+
+-- Creating table customer_address
+if object_id('staging_procfit.dbo.st_customer_address') is null
+begin
+
+create table staging_procfit.dbo.st_customer_address (
+	entidade	numeric(15)
+	,cidade		varchar(100)
+	,UF			varchar(2)
+)
+end;
+
+
+----------------------------------------------
+--           Trincating tables 
+----------------------------------------------
+truncate table staging_procfit.dbo.st_customer;
+
+truncate table staging_procfit.dbo.st_customer_state;
+
+truncate table staging_procfit.dbo.st_customer_classification;
+
+truncate table staging_procfit.dbo.st_customer_address;
+
+
+
+----------------------------------------------
+--           Ingesting Data 
+----------------------------------------------
+
+-- Copy customers into stage area
+insert into staging_procfit.dbo.st_customer (
+	cod_cliente
+	,nome
+	,nome_fantasia
+	,cod_classificacao
+)
+
+	select 
+		en.entidade									as cod_cliente
+		,en.nome									as nome
+		,en.nome_fantasia							as nome_fantasia
+		,en.classificacao_cliente					as cod_classificacao
+	from procfit.dbo.entidades						en
+;
+
+
+-- Copy customere_state into stage area
+insert into staging_procfit.dbo.st_customer_state (
+	UF
+	,estado
+)
+
+	select 
+		es.estado					as UF
+		,es.nome					as estado
+	from procfit.dbo.estados        es
+;
+
+
+-- Copy customer_classification into stage area
+insert into staging_procfit.dbo.st_customer_classification (
+	cod_classificacao
+	,descricao
+)
+
+	select 
+		cc.classificacao_cliente					as cod_cliente
+		,cc.descricao								as classificacao_descricao
+	from procfit.dbo.classificacoes_clientes        cc
+;
+
+
+-- Copy customer_address into stage area
+insert into staging_procfit.dbo.st_customer_address (
+	entidade
+	,cidade
+	,uf
+)
+
+	select 
+		ed.entidade					as entidade
+		,ed.cidade					as cidade
+		,ed.estado					as estado
+	from procfit.dbo.enderecos        ed
+;
