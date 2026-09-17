8f9631a8a1d5638bdf6eb9a8bcbf878a49e19ff3
ErickVisone <erickvisone@hotmail.com>
2026-09-17 20:49:06 +0200

    feat: adding customer table and procedure

---
+++ SQL/queries/stage/stage_product.sql
@@ -0,0 +1,60 @@
+-- Ingest customer data from transactional database to staging area
+use staging_procfit;
+
+
+-- Creating tables
+if object_id('staging_procfit.dbo.st_product') is null
+begin
+	create table staging_procfit.dbo.st_product (
+        produto                 numeric(15)
+        ,descricao              varchar(100)
+        ,descricao_reduzida     varchar(60)
+        ,familia                varchar(80)
+        ,secao                  varchar(80)
+        ,grupo                  varchar(80)
+        ,sub_grupo              varchar(80)
+        ,marca                  varchar(80)
+	)
+end
+;
+
+
+
+-- Truncate old records
+truncate table staging_procfit.dbo.st_product;
+
+
+
+-- Copy from transactional database into stage area
+-- decision to join more than one table was to create a star_schema model.
+-- as the queries were lightweitgh, this join would not affect the production database at all
+insert into staging_procfit.dbo.st_product (
+	produto
+    ,descricao
+    ,descricao_reduzida
+    ,familia
+    ,secao
+    ,grupo
+    ,sub_grupo
+    ,marca
+	)
+select
+    pr.produto                                      as produto
+    ,pr.descricao                                   as descricao
+    ,pr.descricao_reduzida                          as descricao_reduzida
+    ,pr.familia_produto                             as familia
+    ,sp.secao_produto                               as secao
+    ,gp.grupo_produto                               as grupo
+    ,sg.subgrupo_produto                            as sub_grupo
+    ,ma.descricao                                   as marca
+from procfit.dbo.produtos                            pr
+left join procfit.dbo.familias_produtos              fp
+    on pr.familia_produto = fp.familia_produto
+left join procfit.dbo.secoes_produtos                sp
+    on pr.secao_produto = sp.secao_produto
+left join procfit.dbo.grupos_produtos                gp
+    on pr.grupo_produto = gp.grupo_produto
+left join procfit.dbo.subgrupos_produtos             sg
+    on pr.subgrupo_produto = sg.subgrupo_produto
+left join procfit.dbo.marcas                         ma
+    on pr.marca = ma.marca
